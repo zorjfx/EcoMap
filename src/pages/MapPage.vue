bf1219a9d7db4e8a31b48006d24a4632da5869bc
Roman <r.a.zhilenko08@gmail.com>
2026-04-22 22:27:45 +0500

    Added tree info pop-up card

--- src/pages/MapPage.vue
+++ src/pages/MapPage.vue
@@ -1,6 +1,80 @@
 <template>
   <q-page class="map-page">
-    <div ref="mapEl" class="map"></div>
+    <div ref="mapEl" class="map">
+      <transition name="slide-up">
+        <q-card v-if="selectedTree" class="tree-card no-shadow">
+          <q-card-section class="q-pa-md">
+            <div class="column items-start no-wrap">
+              <div class="column items-start justify-between">
+                <div class="text-h5 oswald text-weight-bold">
+                  {{ selectedTree.name }}
+                </div>
+                <div class="text-subtitle1 inter text-italic">({{ selectedTree.latinName }})</div>
+              </div>
+              <div class="column full-width">
+                <div class="q-mt-sm inter text-subtitle1">
+                  Состояние: {{ selectedTree.health }}%
+                </div>
+
+                <q-linear-progress
+                  :value="selectedTree.health / 100"
+                  color="positive"
+                  track-color="grey-4"
+                  rounded
+                  size="10px"
+                  class="q-mt-sm"
+                />
+              </div>
+            </div>
+
+            <div class="row q-col-gutter-md q-mt-md">
+              <div class="col-6">
+                <q-card flat class="info-box bg-grey-2">
+                  <q-card-section>
+                    <div class="text-h6 oswald text-weight-bold">Эко-ценность</div>
+                    <div class="text-h4 text-green-8 text-weight-bold">
+                      +{{ selectedTree.points }} баллов
+                    </div>
+                  </q-card-section>
+                </q-card>
+
+                <q-card flat class="info-box bg-grey-2 q-mt-md">
+                  <q-card-section>
+                    <div class="text-h6 text-weight-bold oswald">Факт</div>
+                    <div class="q-mt-sm inter">
+                      {{ selectedTree.fact }}
+                    </div>
+                  </q-card-section>
+                </q-card>
+              </div>
+
+              <div class="col-6 full-height">
+                <q-card flat class="info-box bg-grey-2">
+                  <q-card-section>
+                    <div class="inter">Координаты: 30.71°, 36.73°</div>
+                    <div class="inter">Последнее сканирование: 19:39</div>
+                  </q-card-section>
+                </q-card>
+                <q-img
+                  :src="selectedTree.image"
+                  height="210px"
+                  class="rounded-borders q-ma-sm full-width"
+                />
+              </div>
+            </div>
+          </q-card-section>
+
+          <q-btn
+            class="absolute-top-right q-ma-sm"
+            flat
+            round
+            dense
+            icon="close"
+            @click="selectedTree = null"
+          />
+        </q-card>
+      </transition>
+    </div>
   </q-page>
 </template>
 
@@ -9,7 +83,45 @@ import { ref, onMounted, nextTick } from 'vue';
 import type { FeatureCollection, Polygon } from 'geojson';
 import L from 'leaflet';
 
+type Tree = {
+  id: number;
+  name: string;
+  latinName: string;
+  health: number;
+  points: number;
+  lat: number;
+  lng: number;
+  image: string;
+  fact: string;
+};
+
+const trees: Tree[] = [
+  {
+    id: 15,
+    name: 'Береза повислая',
+    latinName: 'Betula pendula',
+    health: 80,
+    points: 100,
+    lat: 43.2393,
+    lng: 76.8884,
+    image: 'src/media/bereza.jpg',
+    fact: 'Эта береза поглощает 20 кг CO₂ в год и увлажняет воздух для всего двора!',
+  },
+  {
+    id: 16,
+    name: 'Дуб черешчатый',
+    latinName: 'Quercus robur',
+    health: 72,
+    points: 120,
+    lat: 43.2398,
+    lng: 76.8892,
+    image: 'src/media/dub.jpg',
+    fact: 'Дубы поддерживают биоразнообразие и живут десятки лет.',
+  },
+];
+
 const mapEl = ref<HTMLElement | null>(null);
+const selectedTree = ref<Tree | null>(null);
 
 onMounted(async () => {
   await nextTick();
@@ -96,7 +208,14 @@ onMounted(async () => {
     }),
   }).addTo(map);
 
-  L.marker([43.2393, 76.8884], { icon: treeIcon }).addTo(map);
+  trees.forEach((tree) => {
+    const marker = L.marker([tree.lat, tree.lng], { icon: treeIcon }).addTo(map);
+
+    marker.on('click', () => {
+      selectedTree.value = tree;
+      console.log(selectedTree.value);
+    });
+  });
 
   setTimeout(() => {
     map.invalidateSize();
@@ -108,10 +227,39 @@ onMounted(async () => {
 .map-page {
   width: 100%;
   height: 100vh;
+  position: relative;
 }
 
 .map {
   width: 100%;
   height: 100%;
+}
+
+.tree-card {
+  position: absolute;
+  left: 12px;
+  right: 12px;
+  bottom: 12px;
+  z-index: 1000;
+
+  max-height: 85vh;
+  overflow-y: auto;
+  border-radius: 28px;
+  background: rgba(255, 255, 255, 0.97);
+}
+
+.info-box {
+  border-radius: 18px;
+}
+
+.slide-up-enter-active,
+.slide-up-leave-active {
+  transition: all 0.25s ease;
+}
+
+.slide-up-enter-from,
+.slide-up-leave-to {
+  opacity: 0;
+  transform: translateY(30px);
 }
 </style>
